470b5904ae8c83f4077f8fb5240ded582634c333
Denys Huch <denyshuch@gmail.com>
2026-04-23 14:49:33 +0300

    Clear

--- Runtime/Proxy/Common/ParseErrorResponse.cs
+++ Runtime/Proxy/Common/ParseErrorResponse.cs
@@ -1,5 +1,4 @@
 using System;
-using Newtonsoft.Json;
 
 namespace Playserv.Proxy.Common
 {
@@ -9,20 +8,14 @@ namespace Playserv.Proxy.Common
     [Serializable]
     public sealed class ParseErrorResponse
     {
-        [JsonProperty("error")]
-        private string error;
-
-        [JsonProperty("receivedJson")]
-        private string receivedJson;
-
         /// <summary>
         /// Parse error message.
         /// </summary>
-        public string Error => error;
+        public string Error { get; set; }
 
         /// <summary>
         /// Raw JSON string that failed to parse.
         /// </summary>
-        public string ReceivedJson => receivedJson;
+        public string ReceivedJson { get; set; }
     }
 }
