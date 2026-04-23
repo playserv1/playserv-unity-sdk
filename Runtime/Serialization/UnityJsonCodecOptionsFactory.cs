4b0fdc4210d9e205999a56c5fedc65a98afd1e63
Denys Huch <denyshuch@gmail.com>
2026-04-23 15:08:51 +0300

    Clean Newton JSON using

---
+++ Runtime/Serialization/UnityJsonCodecOptionsFactory.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace Playserv.Serialization
+{
+    internal static class UnityJsonCodecOptionsFactory
+    {
+        public static JsonCodecOptions CreateDefaultEventOptions()
+        {
+            var converters = new List<object>();
+#if UNITY_5_3_OR_NEWER
+            converters.Add(new Vector3NewtonsoftJsonConverter());
+            converters.Add(new QuaternionNewtonsoftJsonConverter());
+#endif
+            return new JsonCodecOptions
+            {
+                IncludeNullValues = true,
+                IgnoreMissingMembers = true,
+                ParseDates = true,
+                CustomConverters = converters
+            };
+        }
+    }
+}
